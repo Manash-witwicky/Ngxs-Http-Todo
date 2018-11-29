7efffb91d58665bd058a448031f92a878ae0146f
mandas <manash.das@syncron.com>
2018-11-29 15:57:42 +0530

    Comment added on Todo component file

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoState } from './../state/todo.state';
-import { FetchTodo, RemoveTodo } from './../state/todo.actions';
-import { Store , Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { FetchTodo, RemoveTodo } from './../state/todo.actions';
+import { TodoState } from './../state/todo.state';
 
-
+// Added By Manash
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-
   @Select(TodoState.load)
   todo$: Observable<any>;
 
-  constructor(private store: Store) { }
+  constructor(private store: Store) {}
 
   deletetodo(id) {
     this.store.dispatch(new RemoveTodo(id));
@@ -24,5 +23,4 @@ export class TodoComponent implements OnInit {
   ngOnInit() {
     this.store.dispatch(new FetchTodo());
   }
-
 }
